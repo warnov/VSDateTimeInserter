9f22b2dbe436b0c1ab6dfa7b9d58fe6ca795a269
warnov <warnov@gmail.com>
2014-07-02 19:33:12 -0500

    Adding files from first state after running the Wizard

---
+++ VSDateTimeInserter/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace WarNov.VSDateTimeInserter
+{
+    static class GuidList
+    {
+        public const string guidVSDateTimeInserterPkgString = "17729dc5-d246-410f-87eb-e285732f045b";
+        public const string guidVSDateTimeInserterCmdSetString = "18e2f04b-d31e-4f4c-b117-d007ad6ff143";
+
+        public static readonly Guid guidVSDateTimeInserterCmdSet = new Guid(guidVSDateTimeInserterCmdSetString);
+    };
+}
---
+++ VSDateTimeInserter/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace WarNov.VSDateTimeInserter
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdInsertDateTime =        0x100;
+
+
+    };
+}
